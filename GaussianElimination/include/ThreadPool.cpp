6d341810d0ff0acd5eb420ab911c4e4431c08a74
Ezra Wu <ezra40907@gmail.com>
2023-06-16 22:53:00 +0800

    fix WaitAll Bug. (but still not stable)

--- GaussianElimination/include/ThreadPool.cpp
+++ GaussianElimination/include/ThreadPool.cpp
@@ -5,20 +5,26 @@
 #include <mutex>
 #include <condition_variable>
 
-inline ThreadPool::ThreadPool(size_t numThreads) : stop(false) {
+ThreadPool::ThreadPool(size_t numThreads,size_t totalTasks) : stop(false), taskCount(0), started(false), submittedTasks(0),totalTasks(totalTasks) {
+
 	for (size_t i = 0; i < numThreads; i++) {
 		threads.emplace_back([this] {
 			while (true) {
 				std::function<void()> task;
 				{
+					started = true;
 					std::unique_lock<std::mutex> lock(queueMutex);
 					condition.wait(lock, [this] { return stop || !tasks.empty(); });
 					if (stop && tasks.empty())
 						return;
 					task = std::move(tasks.front());
 					tasks.pop();
+					submittedTasks++;
+					taskCount++;
 				}
 				task();
+				taskCount--;
+				taskFinishedCondition.notify_all();
 			}
 			});
 	}
@@ -28,25 +34,14 @@ ThreadPool::~ThreadPool() {
 	{
 		std::unique_lock<std::mutex> lock(queueMutex);
 		stop = true;
+		started = false;
 	}
 	condition.notify_all();
 	for (std::thread& thread : threads)
 		thread.join();
 }
 
-void ThreadPool::WaitAll()
-{
+void ThreadPool::WaitAll() {
 	std::unique_lock<std::mutex> lock(queueMutex);
-	condition.wait(lock, [this] {return tasks.empty(); });
+	taskFinishedCondition.wait(lock, [this] { return taskCount == 0 && submittedTasks == totalTasks; });
 }
-
-template<class T>
-void ThreadPool::Enqueue(T&& t)
-{
-	{
-		std::unique_lock<std::mutex> lock(queueMutex);
-		tasks.emplace(std::forward<T>(t));
-	}
-	condition.notify_one();
-}
-
--- GaussianElimination/include/ThreadPool.h
+++ GaussianElimination/include/ThreadPool.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Barrier.h"
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -9,16 +10,30 @@
 
 class ThreadPool {
 public:
-	ThreadPool(size_t numThreads);
+	ThreadPool(size_t numThreads,size_t);
 	~ThreadPool();
 	template<class T>
 	void Enqueue(T&&);
 	void WaitAll();
-
 private:
 	std::vector<std::thread> threads;
 	std::queue<std::function<void()>> tasks;
 	std::mutex queueMutex;
 	std::condition_variable condition;
+	int taskCount;
+	size_t submittedTasks;
+	size_t totalTasks;
+	std::condition_variable taskFinishedCondition;
 	bool stop;
+	bool started;
 };
+
+template<class T>
+void ThreadPool::Enqueue(T&& t)
+{
+	{
+		std::unique_lock<std::mutex> lock(queueMutex);
+		tasks.emplace(std::forward<T>(t));
+	}
+	condition.notify_one();
+}
