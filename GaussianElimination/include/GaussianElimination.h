a5ab8e0b15af4c6f3bec4181cec8e124aeb84f67
Ezra Wu <ezra40907@gmail.com>
2023-06-16 11:29:40 +0800

    1.Added Class MatrixMath for matrix operations. 2.Added two methods in Class MatrixMath:     a. Added GaussianElimination() for performing Gaussian elimination (can be executed in single-threaded or multi-threaded mode).     b. Added CalculateDeterminant() for calculating the determinant of a matrix and returning the result (utilizes multi-threading internally for faster execution).

--- GaussianElimination/include/GaussianElimination.h
+++ GaussianElimination/include/GaussianElimination.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <vector>
 
-class GaussianElimination {
-public:
-};
 
 class Matrix {
 public:
-	Matrix(size_t rows, size_t cols) :rows_(rows), cols_(cols), data_(rows, std::vector<float>(cols, 0.0f)) {}
+	Matrix(size_t rows, size_t cols);
 	Matrix operator+(const Matrix& other) const;
 	float& operator()(size_t row, size_t column);
 	size_t getRows() const;
@@ -16,4 +13,13 @@ private:
 	size_t rows_;
 	size_t cols_;
 	std::vector<std::vector<float>> data_;
+};
+
+class MatrixMath {
+public:
+	static void GaussianElimination(Matrix&, int);
+	static void GaussianElimination(Matrix&);
+	float CalculateDeterminant(Matrix&);
+private:
+
 };
