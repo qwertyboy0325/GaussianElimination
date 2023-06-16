2363e9854d6cca201da6e6d2171537849bad087b
Ezra Wu <ezra40907@gmail.com>
2023-06-16 22:48:56 +0800

    Add phase.

--- GaussianElimination/include/Barrier.cpp
+++ GaussianElimination/include/Barrier.cpp
@@ -1,18 +1,22 @@
 ﻿#include "Barrier.h"
 
-Barrier::Barrier(int numThreads) : numThreads(numThreads), count(0) {}
+Barrier::Barrier(int numThreads) : numThreads(numThreads), count(0),phase(0) {}
 
 void Barrier::wait() {
-	std::unique_lock<std::mutex> lock(mutex);
+    std::unique_lock<std::mutex> lock(mutex);
 
-	count++;
+    size_t currentPhase = phase;
 
-	if (count < numThreads) {
-		// 等待其他線程到達障礙
-		condition.wait(lock, [this] { return count == numThreads; });
-	}
-	else {
-		// 最後一個線程到達障礙，發出信號唤醒所有線程
-		condition.notify_all();
-	}
+    count++;
+
+    if (count < numThreads) {
+        // 等待其他線程到達障礙
+        condition.wait(lock, [this, currentPhase] { return phase != currentPhase; });
+    }
+    else {
+        // 最後一個線程到達障礙，發出信號唤醒所有線程
+        phase++;
+        count = 0;
+        condition.notify_all();
+    }
 }
