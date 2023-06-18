52211ee9fad8a428c651346ea29df12fdb165bed
Ezra Wu <ezra40907@gmail.com>
2023-06-18 21:40:48 +0800

    change folder name main -> src

---
+++ GaussianElimination/src/main.cpp
@@ -0,0 +1,17 @@
+#define DEBUG_SPEEDTEST
+#define HIDE_MATRIX
+
+#include <iostream>
+#include <thread>
+#include <random>
+#include <chrono>
+#include <string>
+#include "../include/ThreadPool.h"
+#include "../include/Matrix.h"
+#include "../include/Barrier.h"
+#include "./test/performance_test.h"
+
+int main()
+{
+
+}
